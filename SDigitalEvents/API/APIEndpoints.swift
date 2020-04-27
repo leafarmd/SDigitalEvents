04e827d57b749d49e6daae80c86f5cfb426eb340
Rafael <leafarmd@gmail.com>
2020-04-27 20:04:35 -0300

    [FEAT]: APICore and APIEndpoints created

--- SDigitalEvents/API/APIEndpoints.swift
+++ SDigitalEvents/API/APIEndpoints.swift
@@ -6,18 +6,20 @@ enum APIEndpoint {
 
 extension APIEndpoint {
     
-    var baseUrl: String {
-        return "https://5b840ba5db24a100142dcd8c.mockapi.io/api/"
-    }
+    var baseUrl: String { "https://5b840ba5db24a100142dcd8c.mockapi.io/api/" }
     
     var url: String {
         switch self {
-        case .events:
-            return "\(baseUrl)events"
-        case .eventDetail(let id):
-            return "\(baseUrl)events/\(id)"
-        case .checkIn:
-            return "\(baseUrl)checkin"
+        case .events:  return "\(baseUrl)events"
+        case .eventDetail(let id): return "\(baseUrl)events/\(id)"
+        case .checkIn: return "\(baseUrl)checkin"
+        }
+    }
+    
+    var method: HttpMethod {
+        switch  self {
+        case .events, .eventDetail(_): return .GET
+        case .checkIn: return .POST
         }
     }
 }
