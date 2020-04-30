919961ab10130085fced183c8468f616cbf943bd
Rafael <leafarmd@gmail.com>
2020-04-29 21:12:05 -0300

    [FIX]: Updated Loader for warnings messages fixes

--- SDigitalEvents/Components/LoadingHandler.swift
+++ SDigitalEvents/Components/LoadingHandler.swift
@@ -1,24 +1,14 @@
-//
-//  Handlers.swift
-//  Test-iOS
-//
-//  Created by Rafael Damasceno on 27/07/19.
-//  Copyright © 2019 Rafael Damasceno. All rights reserved.
-//
-
 import UIKit
 
 struct Loading {
-    
     private static let loadingView = UIView(frame: UIScreen.main.bounds)
-    private static let activityIndicator = UIActivityIndicatorView(style: .white)
+    private static let activityIndicator = UIActivityIndicatorView(style: .medium)
     
     private static func createLoadingView() {
         loadingView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3)
         
         activityIndicator.alpha = 1.0
-        activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2,
-                                           y: loadingView.frame.size.height / 2)
+        activityIndicator.center = CGPoint(x: loadingView.frame.size.width / 2, y: loadingView.frame.size.height / 2)
         
         loadingView.addSubview(activityIndicator)
         loadingView.bringSubviewToFront(activityIndicator)
@@ -27,12 +17,13 @@ struct Loading {
     static func start() {
         createLoadingView()
         activityIndicator.startAnimating()
-        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        UIApplication.shared.keyWindow?.addSubview(loadingView)
+        
+        let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow })
+        
+        window?.addSubview(loadingView)
     }
     
     static func stop() {
-        UIApplication.shared.isNetworkActivityIndicatorVisible = false
         activityIndicator.stopAnimating()
         activityIndicator.removeFromSuperview()
         loadingView.removeFromSuperview()
